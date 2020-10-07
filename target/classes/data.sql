2a1342b4069db540891cfbc99359b6f2c5acff2f
Shuo Wang <wangshuo28@gmail.com>
2020-10-06 21:04:41 -0700

    updated gitignore. added target file

---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, '1970-10-1', 'mike@gmail.com','John', 0, 'Mike', 'dfaDFE33fdas#');
+insert into user values(2, '1904-2-3', 'smith@hotmail.com', 'Smith', 0, 'John', 'dafDFE#R@3d');
+insert into user values(3, '1948-3-5', 'hi@gmail.com', 'Doe', 1, 'Peter', 'wwafe3rfE3$'); 
